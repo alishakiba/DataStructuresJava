23be2c68e9c958a96eab6cf4fb7a3ff5ed39001d
Ali Shakiba <a.shakiba.iran@gmail.com>
2021-10-21 13:49:49 +0330

    introducing assertions done

--- src/test/ir/ac/vru/array/TestUnitTesting.java
+++ src/test/ir/ac/vru/array/TestUnitTesting.java
@@ -3,6 +3,8 @@ package test.ir.ac.vru.array;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,7 +12,7 @@ public class TestUnitTesting {
     @Test
     @DisplayName("Check whether 1 not equals to 2")
     public void testBasicAssertion() {
-        Assertions.assertNotEquals(1, 1);
+        Assertions.assertNotEquals(1, 2);
     }
 
     @Test
@@ -19,9 +21,9 @@ public class TestUnitTesting {
         int[] numbers = {1,2,3,4,5};
         assertEquals(1, numbers[0]);
         assertEquals(2, numbers[1]);
-        assertEquals(7, numbers[2]);
+        assertEquals(3, numbers[2]);
         assertEquals(4, numbers[3]);
-        assertEquals(8, numbers[4]);
+        assertEquals(5, numbers[4]);
     }
 
     @Test
@@ -31,9 +33,20 @@ public class TestUnitTesting {
         Assertions.assertAll(
                 () -> assertEquals(1, numbers[0]),
                 () -> assertEquals(2, numbers[1]),
-                () -> assertEquals(7, numbers[2]),
+                () -> assertEquals(3, numbers[2]),
                 () -> assertEquals(4, numbers[3]),
-                () -> assertEquals(8, numbers[4])
+                () -> assertEquals(5, numbers[4])
         );
+    }
+
+    @ParameterizedTest (name = "{0}")
+    @DisplayName("Parameterized test is introduced")
+    @ValueSource(ints = {2,3,4,5,6})
+    public void testParameterizedTest(int i) {
+        int j = i + 1;
+        if (i < 4)
+            assertEquals(i+1, j);
+        else
+            assertEquals(i+1, i);
     }
 }
